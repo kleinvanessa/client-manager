31dd65d2d12d80a55c27028e8e02c342c532833e
Vanessa Klein <VANESSAL.K@hotmail.com>
2024-07-27 15:11:41 -0300

    Add repositories

---
+++ ClientManager.Backend/Repositories/ClientRepository .cs
@@ -0,0 +1,61 @@
+﻿using ClientManager.Backend.Data;
+using ClientManager.Backend.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClientManager.Backend.Repositories
+{
+    public class ClientRepository : IClientRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ClientRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> EmailExists(string email)
+        {
+            return await _context.Clients
+            .AnyAsync(c => c.Email == email);
+        }
+
+        public async Task<bool> CpfCnpjExists(string cpfCnpj)
+        {
+            return await _context.Clients
+            .AnyAsync(c => c.CpfCnpj == cpfCnpj);
+        }
+
+        public async Task<bool> StateRegistrationExists(string stateRegistration)
+        {
+            return await _context.Clients
+                .AnyAsync(c => c.StateRegistration == stateRegistration);
+        }
+
+        public async Task<IEnumerable<Client>> GetAll()
+        {
+            return await _context.Clients.ToListAsync();
+        }
+
+        public async Task Add(Client client)
+        {
+            _context.Clients.Add(client);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Update(Client client)
+        {
+            _context.Clients.Update(client);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(int id)
+        {
+            var client = await _context.Clients.FindAsync(id);
+            if (client != null)
+            {
+                _context.Clients.Remove(client);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ ClientManager.Backend/Repositories/IClientRepository.cs
@@ -0,0 +1,15 @@
+﻿using ClientManager.Backend.Entities;
+
+namespace ClientManager.Backend.Repositories
+{
+    public interface IClientRepository
+    {
+        Task<bool> EmailExists(string email);
+        Task<bool> CpfCnpjExists(string cpfCnpj);
+        Task<bool> StateRegistrationExists(string stateRegistration);
+        Task<IEnumerable<Client>> GetAll();
+        Task Add(Client client);
+        Task Update(Client client);
+        Task Delete(int id);
+    }
+}
